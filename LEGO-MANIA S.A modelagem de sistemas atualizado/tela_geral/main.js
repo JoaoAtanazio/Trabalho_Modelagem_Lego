d509aae05ea000b5f5f12361da8a560da4213c00
gustavoboeing12 <gustavofratonib@gmail.com>
2025-05-16 14:14:16 -0300

    versão 12
    ajuste do horário

--- LEGO-MANIA S.A modelagem de sistemas atualizado/tela_geral/main.js
+++ LEGO-MANIA S.A modelagem de sistemas atualizado/tela_geral/main.js
@@ -442,28 +442,3 @@ async function uploadFotoPerfil(file) {
     Telefone: '47 98432-9882'
   }
 ]
-
-document.addEventListener('DOMContentLoaded', function () {
-  function atualizarHorario() {
-    const elementoHorario = document.querySelector('.horario');
-    if (!elementoHorario) return;
-
-    const agora = new Date();
-
-    const dia = String(agora.getDate()).padStart(2, '0');
-    const mes = String(agora.getMonth() + 1).padStart(2, '0');
-    const ano = agora.getFullYear();
-
-    const horas = String(agora.getHours()).padStart(2, '0');
-    const minutos = String(agora.getMinutes()).padStart(2, '0');
-    const segundos = String(agora.getSeconds()).padStart(2, '0');
-
-    elementoHorario.innerHTML = `<p>Data: ${dia}/${mes}/${ano}  Hora: ${horas}:${minutos}:${segundos}</p>`;
-  }
-
-  atualizarHorario();
-  setInterval(atualizarHorario, 1000);
-});
-
-
-
