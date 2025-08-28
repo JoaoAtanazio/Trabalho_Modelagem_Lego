47817429b9e35ed24794b0fa13d4e6716a8dcfd5
gustavoboeing12 <gustavofratonib@gmail.com>
2025-08-28 14:13:24 -0300

    Retirada do botão "lembrar de mim"

--- LEGO-MANIA_S.A/index.php
+++ LEGO-MANIA_S.A/index.php
@@ -69,12 +69,6 @@
             </div>
 
             <div class="d-flex justify-content-between align-items-center mb-3">
-                <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="remember">
-                <label class="form-check-label" for="remember">
-                    Lembrar-me
-                </label>
-                </div>
                 <a href="esqueceu_senha.php" class="text-decoration-none">Esqueceu a senha?</a>
             </div>
 
