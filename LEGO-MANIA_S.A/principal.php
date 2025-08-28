ba3b63ae7d654a03f3386c5b68f70385783cea3f
Guilherme Do Nascimento <guilhermedonascimento157@gmail.com>
2025-08-28 15:50:12 -0300

    Principal
    Não tava indo o nome legal :(

--- LEGO-MANIA_S.A/principal.php
+++ LEGO-MANIA_S.A/principal.php
@@ -1,8 +1,3 @@
-<?php
-    session_start();
-    require_once 'conexao.php';
-?>
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -108,7 +103,7 @@
 
     <!-- Conteúdo -->
     <div class="p-4">
-      <h3>Bem-vindo, <?php echo $_SESSION["usuario"]; ?>!</h3>
+      <h3>Bem-vindo!</h3>
     </div>
   </div>
 </div> 
