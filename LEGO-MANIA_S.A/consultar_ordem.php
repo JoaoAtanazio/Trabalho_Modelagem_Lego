5586812b808d5e387a7ce96ed8bb07f90fcd5fe6
gustavoboeing12 <gustavofratonib@gmail.com>
2025-09-12 17:03:04 -0300

    Retirada do page link

--- LEGO-MANIA_S.A/consultar_ordem.php
+++ LEGO-MANIA_S.A/consultar_ordem.php
@@ -490,25 +490,7 @@ if (isset($_POST['alterar_ordem'])) {
                                 <div>
                                     <span class="text-muted">Mostrando <?= count($ordens) ?> de <?= count($ordens) ?> registros</span>
                                 </div>
-                                <nav>
-                                    <ul class="pagination pagination-sm mb-0">
-                                        <li class="page-item disabled">
-                                            <a class="page-link" href="#"><i class="bi bi-chevron-left"></i></a>
-                                        </li>
-                                        <li class="page-item active">
-                                            <a class="page-link" href="#">1</a>
-                                        </li>
-                                        <li class="page-item">
-                                            <a class="page-link" href="#">2</a>
-                                        </li>
-                                        <li class="page-item">
-                                            <a class="page-link" href="#">3</a>
-                                        </li>
-                                        <li class="page-item">
-                                            <a class="page-link" href="#"><i class="bi bi-chevron-right"></i></a>
-                                        </li>
-                                    </ul>
-                                </nav>
+                                
                             </div>
                         </div>
                     </div>
